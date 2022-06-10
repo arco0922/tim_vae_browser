465500bfc080e9a0e9e14c0041d6f26e095aeca9
arco0922 <arco0922@users.noreply.github.com>
2022-06-10 05:15:21 +0000

    deploy: 1fa1bc1a9fce0b9ba76806df75cc673d8ea26b73

---
+++ _next/static/hhQwwV92d_X6HtmObzaGS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f81c625b297c5ee0.css","static/chunks/pages/index-6fb63da20860d565.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
