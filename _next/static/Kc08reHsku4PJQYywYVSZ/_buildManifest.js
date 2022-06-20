224f8fb65cfa171a20f911181a5c564ca6dea79c
arco0922 <arco0922@users.noreply.github.com>
2022-06-20 05:15:59 +0000

    deploy: ba86e1dfae37e44f13273deabfd728867d70d129

--- _next/static/Kc08reHsku4PJQYywYVSZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f6c693eb2b08093a.css","static/chunks/pages/index-560780c46e848c9f.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
