df6d68c4fec722c4c8cec6dba3729b89997d130c
arco0922 <k9o2u2ta@icloud.com>
2022-07-05 12:46:05 +0900

    feat: add repSounds to project

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -7,6 +7,41 @@ export interface P5WithProps<P> extends p5 {
 export type NumVector = number[];
 export type NumMatrix = number[][];
 
+export type RepSoundId =
+  | 'rs1_00'
+  | 'rs1_01'
+  | 'rs1_02'
+  | 'rs2_00'
+  | 'rs2_01'
+  | 'rs2_02'
+  | 'rs2_03'
+  | 'rs2_04'
+  | 'rs2_05'
+  | 'rs2_06'
+  | 'rs2_07'
+  | 'rs2_08'
+  | 'rs2_09'
+  | 'rs2_10'
+  | 'rs2_11'
+  | 'rs2_12'
+  | 'rs2_13'
+  | 'rs2_14'
+  | 'rs2_15'
+  | 'rs2_16'
+  | 'rs2_17'
+  | 'rs2_18'
+  | 'rs2_19'
+  | 'rs2_20'
+  | 'rs2_21'
+  | 'rs2_22'
+  | 'rs2_23'
+  | 'rs2_24'
+  | 'rs2_25'
+  | 'rs2_26'
+  | 'rs2_27'
+  | 'rs2_28'
+  | 'rs2_29';
+
 export type SampleShapeId =
   | 'ss00'
   | 'ss01'
@@ -44,4 +79,9 @@ export type ShapeParameterKey =
 
 export type ShapeParams = {
   [key in ShapeParameterKey]: number;
+};
+
+export type Point2D = {
+  x: number;
+  y: number;
 };
