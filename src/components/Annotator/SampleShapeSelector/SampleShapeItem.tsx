37a66d85d6ecc72a75c435aab5ab07d23a2f73da
arco0922 <k9o2u2ta@icloud.com>
2022-07-05 12:49:58 +0900

    feat: implement SampleShapeSelector

---
+++ src/components/Annotator/SampleShapeSelector/SampleShapeItem.tsx
@@ -0,0 +1,32 @@
+import { SampleShapeId } from '@app/@types';
+import { url } from '@app/utils/urlConfig';
+import Image from 'next/image';
+import styles from './SampleShapeItem.module.scss';
+
+interface Props {
+  index: number;
+  sampleShapeId: SampleShapeId;
+  selectCallback: () => void;
+}
+
+export const SampleShapeItem = ({
+  index,
+  sampleShapeId,
+  selectCallback,
+}: Props) => {
+  return (
+    <div
+      onClick={selectCallback}
+      className={styles.container}
+    >
+      <p className={styles.index__label}>{index}</p>
+      <Image
+        className={styles.shape__image}
+        src={url(`/imgs/sampleShapes/${sampleShapeId}.jpg`)}
+        alt={`${index} shape`}
+        width={150}
+        height={150}
+      />
+    </div>
+  );
+};
